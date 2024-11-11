d79f3ed146a7dd2636f946a9a89ef3fb99e76cbb
Fred <fredxdur@gmail.com>
2024-11-11 19:13:52 +0100

    yo

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -2,49 +2,52 @@
 
 char **ft_split(char const *s, char c);
 
-static int	ft_len(char *str)
+static int	ft_len(const char *str, char c)
 {
-	int c;
+	int sum;
 
-	c = 0;
+	sum = 0;
 	while (*str == c)
 		str++;
 	while (*str != c && *str != '\0')
 	{
-		c++;
+		sum++;
 		str++;
 	}
-	return(c);
+	return(sum);
 }
 
-static int	ft_wc(char *str, char c)
+static int	ft_wc(const char *str, char c)
 {
 	int	i;
-	int c;
+	int sum;
 
 	i = 0;
-	c = 0;
+	sum = 0;
 	while (str[i] != '\0' )
 	{
 		if (str[i] != c && str[i + 1] == c)
-			c++;
+			sum++;
 		else if (str[i] != c && str[i + 1] == '\0' )
-			c++;
+			sum++;
 		i++;
 	}
-	return(c);
+	return(sum);
+}
 
-static char *ft_putstr(char *d, char *s, char c)
+static char *ft_putstr(const char *s, char c)
 {
 	int	i;
+	char *dest;
 
 	i = 0;
-	if ((d = malloc(sizeof(char) * (ft_len(s) + 1)) == NULL))
+	dest = malloc(sizeof(char) * (ft_len(s, c) + 1));
+	if (dest == NULL)
 		return(NULL);
 	while (*s != c && *s != '\0')
-		d[i++] = *s++;
-	d[i] = '\0';
-	return(d);
+		dest[i++] = *s++;
+	dest[i] = '\0';
+	return(dest);
 }
 
 
@@ -52,18 +55,19 @@ char **ft_split(char const *s, char c)
 {
 	int	i;
 	int	t;
-	char * strs;
+	char **strs;
 
 	i = 0;
 	t = 1;
-	if ((strs = malloc(sizeof(char *) * (ft_wc(s) + 1))) == NULL)
+	if ((strs = malloc(sizeof(char *) * (ft_wc(s, c) + 1))) == NULL)
 		return(NULL);
 	while (*s)
 	{
-		if (*s != c && t = 1)
+		if (*s != c && t == 1)
 		{
-			strs[i++] = ft_putstr(strs[i], s, c);
+			strs[i] = ft_putstr(s, c);
 			t =  0;
+			i++;
 		}
 		if (*s != c && *(s + 1) == c )
 			t = 1;
