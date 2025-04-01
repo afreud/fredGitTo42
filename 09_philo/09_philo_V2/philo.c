88700c0d43429105567a648eb95cdb3d6b236c21
afreud <fredxdur@gmail.com>
2025-04-01 02:59:40 +0200

    Message_Auto

--- 09_philo/09_philo_V2/philo.c
+++ 09_philo/09_philo_V2/philo.c
@@ -24,8 +24,26 @@ void	ft_take_place(t_data *gdata, t_phidata *phi)
 	}
 	pthread_mutex_unlock(&gdata->mutex[PLACE]);
 	usleep(gdata->eat_time * 10);
-	if (gdata->tot_philo == 3)
-		usleep(gdata->eat_time * 10);
+//	if (gdata->tot_philo == 3)
+//		usleep(gdata->eat_time * 10);
+}
+
+void	ft_add_time(t_data *gdata, t_phidata *phi)
+{
+	if (gdata->tot_philo == 3 && phi->meals)
+	{
+		if ((2 * gdata->eat_time) >= gdata->sleep_time)
+			phi->clock += 2 * gdata->eat_time;
+		else
+			phi->clock += gdata->sleep_time;
+	}
+	else if (phi->meals)
+	{
+		if (gdata->eat_time >= gdata->sleep_time)
+			phi->clock += gdata->eat_time;
+		else
+			phi->clock += gdata->sleep_time;
+	}
 }
 
 bool	ft_take_frk(t_data *gdata, t_phidata *phi)
@@ -33,20 +51,6 @@ bool	ft_take_frk(t_data *gdata, t_phidata *phi)
 	pthread_mutex_lock(&gdata->mutex[ALLFKS]);
 	if (gdata->frk[phi->n] == ON_TABLE && gdata->frk[(phi->n + 1) % gdata->tot_philo] == ON_TABLE)
 	{
-		if (gdata->tot_philo == 3 && phi->meals)
-		{
-			if ((2 * gdata->eat_time) >= gdata->sleep_time)
-				phi->clock += 2 * gdata->eat_time;
-			else
-				phi->clock += gdata->sleep_time;
-		}
-		else if (phi->meals)
-		{
-			if (gdata->eat_time >= gdata->sleep_time)
-				phi->clock += gdata->eat_time;
-			else
-				phi->clock += gdata->sleep_time;
-		}
 		gdata->frk[(phi->n + 1) % gdata->tot_philo] = TAKEN;
 		gdata->frk[phi->n] = TAKEN;
 		phi->have2frks = 1;
@@ -73,8 +77,8 @@ void	ft_eat_and_sleep(t_data *gdata, t_phidata *phi)
 		pthread_mutex_unlock(&gdata->mutex[PRINT]);
 
 		usleep(gdata->eat_time * 10);
-		if (gdata->tot_philo == 3)
-			usleep(gdata->eat_time * 10);
+//		if (gdata->tot_philo == 3)
+//			usleep(gdata->eat_time * 10);
 
 		phi->clock_eat = phi->clock;
 		phi->clock += gdata->eat_time;
@@ -89,8 +93,8 @@ void	ft_eat_and_sleep(t_data *gdata, t_phidata *phi)
 		pthread_mutex_unlock(&gdata->mutex[PRINT]);
 
 		usleep(gdata->sleep_time * 10);
-		if (gdata->tot_philo == 3)
-			usleep(gdata->sleep_time * 10);
+//		if (gdata->tot_philo == 3)
+//			usleep(gdata->sleep_time * 10);
 
 		pthread_mutex_lock(&gdata->mutex[PRINT]);
 		printf("%ld %d is thinking\n", (phi->clock + gdata->sleep_time), (phi->n + 1));
@@ -103,14 +107,21 @@ int	ft_check_death(t_data *gdata, t_phidata *phi)
 	if (phi->clock - phi->clock_eat > gdata->death_time)
 	{
 		pthread_mutex_lock(&gdata->mutex[DEATH]);
+		gdata->one_is_dead = 1;
+		pthread_mutex_unlock(&gdata->mutex[DEATH]);
+
+		//usleep(gdata->sleep_time * 10);
 		pthread_mutex_lock(&gdata->mutex[PRINT]);
 		printf("%ld %d is dead\n", phi->clock, (phi->n + 1));
 		pthread_mutex_unlock(&gdata->mutex[PRINT]);
-		gdata->one_is_dead = 1;
-		pthread_mutex_unlock(&gdata->mutex[DEATH]);
 	}
+	pthread_mutex_lock(&gdata->mutex[DEATH]);
 	if (gdata->one_is_dead || phi->meals == gdata->meals_nbr)
+	{
+		pthread_mutex_unlock(&gdata->mutex[DEATH]);
 		return (1);
+	}
+	pthread_mutex_unlock(&gdata->mutex[DEATH]);
 	return (0);
 }
 
@@ -124,12 +135,16 @@ int	ft_dining_philosophers(void *data)
 	ft_take_place(gdata, &phi);
 	while (!gdata->one_is_dead && (!gdata->meals_nbr || (phi.meals < gdata->meals_nbr)))
 	{
-		while (ft_take_frk(gdata, &phi))
-			;
-		ft_eat_and_sleep(gdata, &phi);
+		ft_add_time(gdata, &phi);
 
 		if (ft_check_death(gdata, &phi))
 			return (0);
+
+		while (ft_take_frk(gdata, &phi))
+			;
+
+		ft_eat_and_sleep(gdata, &phi);
+
 	}
 	return (0);
 }
